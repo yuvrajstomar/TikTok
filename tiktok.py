87a87d8bdfff18a93878d72da13e79e9515d374d
Srinjoy Chakravarty <schakravarty@prometheum.com>
2020-11-29 05:53:27 -0500

    :hammer: updated yaml file without prefix

--- tiktok.py
+++ tiktok.py
@@ -153,8 +153,3 @@ if __name__ == "__main__":
 
     # for video in user_vid_list[0]
     #     print(video)
-
-'''
-<a href="https://www.tiktok.com/@jugglinjosh/video/6898091882652486918" class="jsx-3109748587 video-feed-item-wrapper"><span class="lazyload-wrapper"><div class="jsx-828470630 image-card" style="border-radius: 0px; background-image: url(&quot;https://p16-sign-va.tiktokcdn.com/obj/tos-maliva-p-0068/022bd1bcb0c74a2bbeb1f1117cdb0839?x-expires=1606467600&amp;x-signature=EBWO4iKRmSskdqukz%2FC70njrPbI%3D&quot;);"><div class="jsx-1778455663 video-card default"><div class="jsx-1778455663 video-card-mask"><div class="jsx-1036923518 card-footer normal no-avatar"><div class="jsx-1036923518 video-bottom-info"><svg class="like-icon" width="18" height="18" viewBox="0 0 48 48" fill="#fff" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M16 10.554V37.4459L38.1463 24L16 10.554ZM12 8.77702C12 6.43812 14.5577 4.99881 16.5569 6.21266L41.6301 21.4356C43.5542 22.6038 43.5542 25.3962 41.6301 26.5644L16.5569 41.7873C14.5577 43.0012 12 41.5619 12 39.223V8.77702Z"></path></svg><strong class="jsx-1036923518 video-count">69.2K</strong></div></div></div></div></div></span></a>
-'''
-
